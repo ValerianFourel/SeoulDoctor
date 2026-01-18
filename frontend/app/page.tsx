529cccc9a55447ce238908724f5131fc332ad0d2
ValerianFourel <valerian.fourel@yahoo.com>
2026-01-18 16:17:46 +0100

    refactoring backend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,12 @@
+import type { Metadata } from "next";
 import ChatInterface from "../components/ChatInterface";
+
+export const metadata: Metadata = {
+  title: "Home", // You can customize the page title here
+  icons: {
+    icon: "/img/logo.ico",
+  },
+};
 
 export default function Home() {
   return <ChatInterface />;
